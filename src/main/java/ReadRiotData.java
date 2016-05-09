d8751880d9f2e1b2966b2298d3435c98ec28f5dd
Kunal M <kum2104@columbia.edu>
2016-05-08 20:43:28 -0400

    Added more functions

--- src/main/java/ReadRiotData.java
+++ src/main/java/ReadRiotData.java
@@ -1,23 +1,44 @@
 import net.rithms.riot.api.RiotApi;
 import net.rithms.riot.api.RiotApiException;
+import net.rithms.riot.constant.PlatformId;
 import net.rithms.riot.constant.Region;
+import net.rithms.riot.dto.Champion.ChampionList;
+import net.rithms.riot.dto.ChampionMastery.ChampionMastery;
 import net.rithms.riot.dto.Summoner.Summoner;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
  * Created by Kunal on 5/8/2016.
  */
 public class ReadRiotData {
+    private static final RiotApi api = new RiotApi("b45a7774-67ad-4c25-ad84-33f5462f098b");
+    private static final Map<Long, String> championLookup = new HashMap<>();
+
+    private static void createChampionLookup() throws RiotApiException {
+        ChampionList championList = new ChampionList();
+        championList.forEach((k, v) -> System.out.println(k + "," + v));
+
+
+    }
 
     public static void main(String[] args) throws RiotApiException {
+        ReadRiotData data = new ReadRiotData();
+        createChampionLookup();
+        //data.getChampionMasteries("falseno9");
+    }
 
-        RiotApi api = new RiotApi("b45a7774-67ad-4c25-ad84-33f5462f098b");
+    public void getChampionMasteries(String summonerName) throws RiotApiException {
+        List<ChampionMastery> championMasteries = api.getChampionMasteries(PlatformId.NA, getSummonerId(summonerName));
+        championMasteries.forEach(championMastery -> System.out.println(championMastery.getChampionId()));
+    }
 
-        Map<String, Summoner> summoners = api.getSummonersByName(Region.NA, "falseno9, orianna");
-        Summoner summoner = summoners.get("falseno9");
-        long id = summoner.getId();
-        System.out.println(id);
+    private long getSummonerId(String summonerName) throws RiotApiException {
+        Map<String, Summoner> summoners = api.getSummonersByName(Region.NA, summonerName);
+        Summoner summoner = summoners.get(summonerName);
+        return summoner.getId();
     }
 }
 
